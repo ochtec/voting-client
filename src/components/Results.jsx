a70c80b0028437759f129db8248ce5c2726e38d2
miketoc4 <miketoc6@gmail.com>
2016-03-12 18:56:46 -0500

    front-end code done, tests pass

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import Winner from './Winner';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
