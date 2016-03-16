425101f0123fb56f58d85a3260c29abdd8f292f7
miketoc4 <miketoc6@gmail.com>
2016-03-16 17:53:07 -0400

    socketio and connection to server

--- src/components/Vote.jsx
+++ src/components/Vote.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
-export default React.createClass({
+export default React.createClass({  
   mixins: [PureRenderMixin],
   getPair: function() {
     return this.props.pair || [];
@@ -14,16 +14,16 @@ export default React.createClass({
   },
   render: function() {
     return <div className="voting">
-    {this.getPair().map(entry => 
-      <button key={entry}
-              disabled={this.isDisabled()}
-              onClick={() => this.props.vote(entry)}>
-        <h1>{entry}</h1>
-        {this.hasVotedFor(entry) ? 
-          <div className="label">Voted</div> :
-          null}
-      </button>
-    )}
+      {this.getPair().map(entry =>
+        <button key={entry}
+                disabled={this.isDisabled()}
+                onClick={() => this.props.vote(entry)}>
+          <h1>{entry}</h1>
+          {this.hasVotedFor(entry) ?
+            <div className="label">Voted</div> :
+            null}
+        </button>
+      )}
     </div>;
   }
 });
--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import Vote from './Vote';
-import Winner from './Winner';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {connect} from 'react-redux';
+import Winner from './Winner';
+import Vote from './Vote';
+import * as actionCreators from '../action_creators';
 
 export const Voting = React.createClass({
   mixins: [PureRenderMixin],
   render: function() {
-    return <div className="voting">
+    return <div>
       {this.props.winner ?
         <Winner ref="winner" winner={this.props.winner} /> :
         <Vote {...this.props} />}
@@ -18,10 +19,12 @@ export const Voting = React.createClass({
 function mapStateToProps(state) {
   return {
     pair: state.getIn(['vote', 'pair']),
+    hasVoted: state.get('hasVoted'),
     winner: state.get('winner')
   };
 }
 
-connect(mapStateToProps)(Voting);
-
-export default Voting;
+export const VotingContainer = connect(
+  mapStateToProps,
+  actionCreators
+)(Voting);
